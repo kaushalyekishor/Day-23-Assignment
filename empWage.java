6c331da357cf5daf924bdd298494cf2d7a3ce8f1
Kishor <kaushalyekishor1232gmail.com>
2020-07-16 15:21:57 +0530

    emp days reached month code added

--- empWage.java
+++ empWage.java
@@ -159,3 +159,53 @@ public class empWageForMonthSalary
 }
 
 
+
+
+
+//emp Days a Reached for month code added 
+public class empDaysReachedForMonth
+{
+         //Constants
+        public static final int IS_FULL_TIME=1;
+        public static final int IS_PART_TIME=2;
+        public static final int EMP_RATE_PER_HOUR=20;
+        public static final int NUM_OF_WORKING_DAYS=20;
+        public static final int MAX_HRS_IN_MONTH=100;
+
+//              public empTotalWage(int empRate,int numbOfDays,int maxDays)
+//              {
+//                      this.EMP_RATE_PER_HOUR=empRate;
+//                      this.NUM_OF_WORKING_DAYS=numbOfDays;
+//              }
+
+                public static void main(String[ ] args)
+                {
+                        //variables
+                        int empHrs = 0;
+                        int empWage = 0;
+                        int totalEmpWage = 0, totalWorkingDays=0, totalEmpHrs=0;
+                        while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+                        {
+                                totalWorkingDays++;
+                                //int empHrs = 0, empWage = 0;
+                                int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+                                switch (empCheck)
+                                {
+                                case IS_FULL_TIME:
+                                        empHrs = 8;
+                                        break;
+                                case IS_PART_TIME:
+                                        empHrs = 4;
+                                        break;
+                                default:
+                                        empHrs = 0;
+                                }
+                                totalEmpHrs += empHrs;
+                                empWage = empHrs * EMP_RATE_PER_HOUR;
+                                totalEmpWage += empWage;
+                                System.out.println("Employee Wage : "+empWage);
+                        }
+                System.out.println("Total Employee Wage: "+totalEmpWage);
+                }
+}
+
